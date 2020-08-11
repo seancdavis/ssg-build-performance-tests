baca6f1ff26def924a9b022f82fa443ca52fe12d
Sean C Davis <scdavis41@gmail.com>
2020-08-10 20:06:48 -0400

    Add other ssg tests to config

--- test.config.js
+++ test.config.js
@@ -1,9 +1,10 @@
 const path = require("path")
 
 module.exports = {
+  // fileCount: [1, 100],
   fileCount: [1, 100, 1000, 5000, 10000],
-  // fileCount: [1, 100, 1000, 5000, 10000],
-  logFile: path.resolve(__dirname, `tmp/log-${new Date().getTime()}.json`),
+  // logFile: path.resolve(__dirname, `tmp/log-${new Date().getTime()}.json`),
+  logFile: path.resolve(__dirname, `tmp/log.json`),
   tests: [
     {
       name: "Hugo",
@@ -15,6 +16,54 @@ module.exports = {
       commands: {
         clean: "rm -rf public",
         build: "hugo -D"
+      }
+    },
+    {
+      name: "Jekyll",
+      paths: {
+        build: path.join(__dirname, "ssg/jekyll/_site"),
+        content: path.join(__dirname, "ssg/jekyll/_pages"),
+        root: path.join(__dirname, "ssg/jekyll")
+      },
+      commands: {
+        clean: "rm -rf _site && rm -rf .jekyll-cache",
+        build: "bundle exec jekyll build"
+      }
+    },
+    {
+      name: "Gatsby",
+      paths: {
+        build: path.join(__dirname, "ssg/gatsby/public"),
+        content: path.join(__dirname, "ssg/gatsby/src/content"),
+        root: path.join(__dirname, "ssg/gatsby")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build"
+      }
+    },
+    {
+      name: "Eleventy",
+      paths: {
+        build: path.join(__dirname, "ssg/eleventy/dist"),
+        content: path.join(__dirname, "ssg/eleventy/src/pages"),
+        root: path.join(__dirname, "ssg/eleventy")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build"
+      }
+    },
+    {
+      name: "Next",
+      paths: {
+        build: path.join(__dirname, "ssg/next/out"),
+        content: path.join(__dirname, "ssg/next/_pages"),
+        root: path.join(__dirname, "ssg/next")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build && yarn export"
       }
     }
   ]
