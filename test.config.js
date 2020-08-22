dbc25d7029dfbfcb31dae8dc367acbbadc830713
Sean C Davis <scdavis41@gmail.com>
2020-08-22 13:24:19 -0400

    Move logger config into config file

--- test.config.js
+++ test.config.js
@@ -19,66 +19,66 @@ module.exports = {
         clean: "rm -rf public",
         build: "hugo -D"
       }
-    },
-    {
-      name: "jekyll",
-      paths: {
-        build: path.join(__dirname, "ssg/jekyll/_site"),
-        content: path.join(__dirname, "ssg/jekyll/_pages"),
-        root: path.join(__dirname, "ssg/jekyll")
-      },
-      commands: {
-        clean: "rm -rf _site && rm -rf .jekyll-cache",
-        build: "bundle exec jekyll build"
-      }
-    },
-    {
-      name: "gatsby",
-      paths: {
-        build: path.join(__dirname, "ssg/gatsby/public"),
-        content: path.join(__dirname, "ssg/gatsby/src/content"),
-        root: path.join(__dirname, "ssg/gatsby")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn build"
-      }
-    },
-    {
-      name: "eleventy",
-      paths: {
-        build: path.join(__dirname, "ssg/eleventy/dist"),
-        content: path.join(__dirname, "ssg/eleventy/src/pages"),
-        root: path.join(__dirname, "ssg/eleventy")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn build"
-      }
-    },
-    {
-      name: "next",
-      paths: {
-        build: path.join(__dirname, "ssg/next/out"),
-        content: path.join(__dirname, "ssg/next/_pages"),
-        root: path.join(__dirname, "ssg/next")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn build && yarn export"
-      }
-    },
-    {
-      name: "nuxt",
-      paths: {
-        build: path.join(__dirname, "ssg/nuxt/dist"),
-        content: path.join(__dirname, "ssg/nuxt/content"),
-        root: path.join(__dirname, "ssg/nuxt")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn generate"
-      }
     }
+    // {
+    //   name: "jekyll",
+    //   paths: {
+    //     build: path.join(__dirname, "ssg/jekyll/_site"),
+    //     content: path.join(__dirname, "ssg/jekyll/_pages"),
+    //     root: path.join(__dirname, "ssg/jekyll")
+    //   },
+    //   commands: {
+    //     clean: "rm -rf _site && rm -rf .jekyll-cache",
+    //     build: "bundle exec jekyll build"
+    //   }
+    // },
+    // {
+    //   name: "gatsby",
+    //   paths: {
+    //     build: path.join(__dirname, "ssg/gatsby/public"),
+    //     content: path.join(__dirname, "ssg/gatsby/src/content"),
+    //     root: path.join(__dirname, "ssg/gatsby")
+    //   },
+    //   commands: {
+    //     clean: "yarn clean",
+    //     build: "yarn build"
+    //   }
+    // },
+    // {
+    //   name: "eleventy",
+    //   paths: {
+    //     build: path.join(__dirname, "ssg/eleventy/dist"),
+    //     content: path.join(__dirname, "ssg/eleventy/src/pages"),
+    //     root: path.join(__dirname, "ssg/eleventy")
+    //   },
+    //   commands: {
+    //     clean: "yarn clean",
+    //     build: "yarn build"
+    //   }
+    // },
+    // {
+    //   name: "next",
+    //   paths: {
+    //     build: path.join(__dirname, "ssg/next/out"),
+    //     content: path.join(__dirname, "ssg/next/_pages"),
+    //     root: path.join(__dirname, "ssg/next")
+    //   },
+    //   commands: {
+    //     clean: "yarn clean",
+    //     build: "yarn build && yarn export"
+    //   }
+    // },
+    // {
+    //   name: "nuxt",
+    //   paths: {
+    //     build: path.join(__dirname, "ssg/nuxt/dist"),
+    //     content: path.join(__dirname, "ssg/nuxt/content"),
+    //     root: path.join(__dirname, "ssg/nuxt")
+    //   },
+    //   commands: {
+    //     clean: "yarn clean",
+    //     build: "yarn generate"
+    //   }
+    // }
   ]
 }
