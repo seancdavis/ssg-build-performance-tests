59bfbd4379e24549bfd0c713d9b7e91dc7b20736
Sean C Davis <scdavis41@gmail.com>
2020-09-27 07:01:07 -0400

    Run and store tests in datasets

--- test.config.js
+++ test.config.js
@@ -1,13 +1,43 @@
 const path = require("path")
 
 module.exports = {
-  // fileCount: [1, 100, 1000, 5000],
-  fileCount: [10000, 20000],
+  datasets: {
+    base: [1],
+    small: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
+    large: [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000]
+  },
   log: {
     file: path.resolve(__dirname, `src/results.json`),
     maxHistory: 10
   },
   tests: [
+    {
+      name: "eleventy",
+      color: "#222",
+      paths: {
+        build: path.join(__dirname, "ssg/eleventy/dist"),
+        content: path.join(__dirname, "ssg/eleventy/src/pages"),
+        root: path.join(__dirname, "ssg/eleventy")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build"
+      }
+    },
+    {
+      name: "gatsby",
+      color: "#542c85",
+      framework: true,
+      paths: {
+        build: path.join(__dirname, "ssg/gatsby/public"),
+        content: path.join(__dirname, "ssg/gatsby/src/content"),
+        root: path.join(__dirname, "ssg/gatsby")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build"
+      }
+    },
     {
       name: "hugo",
       color: "#ff4088",
@@ -35,34 +65,9 @@ module.exports = {
       }
     },
     {
-      name: "gatsby",
-      color: "#542c85",
-      paths: {
-        build: path.join(__dirname, "ssg/gatsby/public"),
-        content: path.join(__dirname, "ssg/gatsby/src/content"),
-        root: path.join(__dirname, "ssg/gatsby")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn build"
-      }
-    },
-    {
-      name: "eleventy",
-      color: "#222",
-      paths: {
-        build: path.join(__dirname, "ssg/eleventy/dist"),
-        content: path.join(__dirname, "ssg/eleventy/src/pages"),
-        root: path.join(__dirname, "ssg/eleventy")
-      },
-      commands: {
-        clean: "yarn clean",
-        build: "yarn build"
-      }
-    },
-    {
       name: "next",
       color: "#0070f3",
+      framework: true,
       paths: {
         build: path.join(__dirname, "ssg/next/out"),
         content: path.join(__dirname, "ssg/next/_pages"),
@@ -76,6 +81,7 @@ module.exports = {
     {
       name: "nuxt",
       color: "#00c58e",
+      framework: true,
       paths: {
         build: path.join(__dirname, "ssg/nuxt/dist"),
         content: path.join(__dirname, "ssg/nuxt/content"),
