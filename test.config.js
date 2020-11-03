abd03b344f72be8bc2285f0c9fb76cba99576ae8
Sean C Davis <scdavis41@gmail.com>
2020-11-03 07:00:43 -0500

    Enable the rest of the first six

--- test.config.js
+++ test.config.js
@@ -25,20 +25,20 @@ module.exports = {
         build: "yarn build"
       }
     },
-    // {
-    //   name: "gatsby",
-    //   color: "#542c85",
-    //   framework: true,
-    //   paths: {
-    //     build: path.join(__dirname, "ssg/gatsby/public"),
-    //     content: path.join(__dirname, "ssg/gatsby/src/content"),
-    //     root: path.join(__dirname, "ssg/gatsby")
-    //   },
-    //   commands: {
-    //     clean: "yarn clean",
-    //     build: "NODE_OPTIONS=--max_old_space_size=4096 yarn build"
-    //   }
-    // },
+    {
+      name: "gatsby",
+      color: "#542c85",
+      framework: true,
+      paths: {
+        build: path.join(__dirname, "ssg/gatsby/public"),
+        content: path.join(__dirname, "ssg/gatsby/src/content"),
+        root: path.join(__dirname, "ssg/gatsby")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "NODE_OPTIONS=--max_old_space_size=4096 yarn build"
+      }
+    },
     {
       name: "hugo",
       color: "#ff4088",
@@ -51,47 +51,47 @@ module.exports = {
         clean: "rm -rf public",
         build: "hugo -D"
       }
+    },
+    {
+      name: "jekyll",
+      color: "#fc0",
+      paths: {
+        build: path.join(__dirname, "ssg/jekyll/_site"),
+        content: path.join(__dirname, "ssg/jekyll/_pages"),
+        root: path.join(__dirname, "ssg/jekyll")
+      },
+      commands: {
+        clean: "rm -rf _site && rm -rf .jekyll-cache",
+        build: "bundle exec jekyll build"
+      }
+    },
+    {
+      name: "next",
+      color: "#0070f3",
+      framework: true,
+      paths: {
+        build: path.join(__dirname, "ssg/next/out"),
+        content: path.join(__dirname, "ssg/next/_pages"),
+        root: path.join(__dirname, "ssg/next")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn build && yarn export"
+      }
+    },
+    {
+      name: "nuxt",
+      color: "#00c58e",
+      framework: true,
+      paths: {
+        build: path.join(__dirname, "ssg/nuxt/dist"),
+        content: path.join(__dirname, "ssg/nuxt/content"),
+        root: path.join(__dirname, "ssg/nuxt")
+      },
+      commands: {
+        clean: "yarn clean",
+        build: "yarn generate"
+      }
     }
-    // {
-    //   name: "jekyll",
-    //   color: "#fc0",
-    //   paths: {
-    //     build: path.join(__dirname, "ssg/jekyll/_site"),
-    //     content: path.join(__dirname, "ssg/jekyll/_pages"),
-    //     root: path.join(__dirname, "ssg/jekyll")
-    //   },
-    //   commands: {
-    //     clean: "rm -rf _site && rm -rf .jekyll-cache",
-    //     build: "bundle exec jekyll build"
-    //   }
-    // },
-    // {
-    //   name: "next",
-    //   color: "#0070f3",
-    //   framework: true,
-    //   paths: {
-    //     build: path.join(__dirname, "ssg/next/out"),
-    //     content: path.join(__dirname, "ssg/next/_pages"),
-    //     root: path.join(__dirname, "ssg/next")
-    //   },
-    //   commands: {
-    //     clean: "yarn clean",
-    //     build: "yarn build && yarn export"
-    //   }
-    // },
-    // {
-    //   name: "nuxt",
-    //   color: "#00c58e",
-    //   framework: true,
-    //   paths: {
-    //     build: path.join(__dirname, "ssg/nuxt/dist"),
-    //     content: path.join(__dirname, "ssg/nuxt/content"),
-    //     root: path.join(__dirname, "ssg/nuxt")
-    //   },
-    //   commands: {
-    //     clean: "yarn clean",
-    //     build: "yarn generate"
-    //   }
-    // }
   ]
 }
