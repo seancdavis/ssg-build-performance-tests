158e328028ee3ba52cd6a1efe16ae74f9dff9ebb
Sean C Davis <scdavis41@gmail.com>
2020-11-03 05:44:38 -0500

    Add simple node server

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-const { print } = require("graphql")
-const fetch = require("node-fetch")
-const gql = require("graphql-tag")
+const express = require("express")
+const port = process.env.PORT || 3000
 
-const query = gql`
-  # mutation InsertGenerator($title: String!) {
-  #   insert_generators_one(object: { title: $title }) {
-  #     id
-  #     title
-  #   }
-  # }
-  query GetDataset($title: String) {
-    datasets(where: { title: { _eq: $title } }) {
-      id
-    }
-  }
-`
+const app = express()
 
-async function go() {
-  const response = await fetch("https://ssg-build-performance-tests.herokuapp.com/v1/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "x-hasura-admin-secret": process.env.HASURA_SECRET,
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      query: print(query),
-      variables: { title: Date.now().toString() }
-    })
-  })
-  const { data } = await response.json()
-  console.log(data)
-}
+app.get("/", (req, res) => {
+  res.send("SSG Build Performance Test Runner")
+})
 
-go()
+app.listen(port, () => {
+  console.log(`App listening on ${port}`)
+})
