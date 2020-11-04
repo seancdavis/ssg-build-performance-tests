25d726d794130415410a4662f033c1c6b8f6e13c
Sean C Davis <scdavis41@gmail.com>
2020-11-04 06:02:20 -0500

    Add zero runs to each dataset

--- test.config.js
+++ test.config.js
@@ -2,9 +2,9 @@ const path = require("path")
 
 module.exports = {
   datasets: {
-    base: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    base: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     small: [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
-    large: [1000, 2000, 4000, 8000, 16000, 32000, 64000]
+    large: [0, 1000, 2000, 4000, 8000, 16000, 32000, 64000]
   },
   log: {
     hasura_secret: process.env.HASURA_SECRET,
