cbf79369d1a815d115a491bb7d17977ee3778b18
Sean C Davis <scdavis41@gmail.com>
2021-02-23 10:36:12 -0500

    Add dev dataset

--- test.config.js
+++ test.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 
 module.exports = {
   datasets: {
+    dev: [1],
     base: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     small: [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
     large: [0, 1000, 2000, 4000, 8000, 16000, 32000, 64000]
